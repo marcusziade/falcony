6d34efddbb108c543930fe1332d1db4b73920ea1
Marcus Ziadé <marcus.ziade@ouraring.com>
2023-09-07 10:59:19 +0300

    Use new API and retry

--- go.mod
+++ go.mod
@@ -5,8 +5,21 @@ go 1.21.0
 require github.com/gocolly/colly v1.2.0
 
 require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/chromedp/cdproto v0.0.0-20230802225258-3cf4e6d46a89 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/dop251/goja v0.0.0-20230812105242-81d76064690d // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
+	github.com/google/pprof v0.0.0-20230811205829-9131a7e9cc17 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
@@ -16,10 +29,12 @@ require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.17 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/chromedp/chromedp v0.9.2
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/kkdai/youtube/v2 v2.8.4
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/stretchr/testify v1.8.4
 	github.com/temoto/robotstxt v1.1.2 // indirect
--- main.go
+++ main.go
@@ -1,112 +1,118 @@
 package main
 
 import (
-	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"strings"
+	"time"
 
-	"github.com/gocolly/colly"
+	"github.com/chromedp/chromedp"
+	"github.com/kkdai/youtube/v2"
 )
 
-func readDownloadedTitles(filePath string) (map[string]bool, error) {
-	titles := make(map[string]bool)
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Printf("Failed to open downloaded titles file: %v", err)
-		return titles, nil
-	}
-	defer file.Close()
+const channelURL = "https://www.youtube.com/user/@goodgrief3308/videos"
+const historyFilePath = "videoHistory.json"
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		title := scanner.Text()
-		titles[title] = true
+func getLatestVideoURL(ctx context.Context) (string, string, error) {
+	log.Println("Navigating to channel...")
+	var videoURL, thumbnailURL string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(channelURL),
+		chromedp.WaitVisible(`a#thumbnail`, chromedp.ByQuery),
+		chromedp.AttributeValue(`a#thumbnail img`, "src", &thumbnailURL, nil),
+		chromedp.AttributeValue(`a#video-title-link`, "href", &videoURL, nil),
+	)
+	if videoURL == "" {
+		return "", "", errors.New("could not find video URL")
 	}
-	if err := scanner.Err(); err != nil {
-		log.Printf("Failed to read from file: %v", err)
-		return nil, err
-	}
-
-	return titles, nil
+	return "https://www.youtube.com" + videoURL, thumbnailURL, err
 }
 
-func appendDownloadedTitle(filePath, title string) error {
-	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func handleFatalError(err error, msg string) {
 	if err != nil {
-		log.Printf("Failed to open or create file: %v", err)
-		return err
+		log.Fatalf("%s: %v", msg, err)
 	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	if _, err := writer.WriteString(title + "\n"); err != nil {
-		log.Printf("Failed to write to file: %v", err)
-		return err
-	}
-
-	if err := writer.Flush(); err != nil {
-		log.Printf("Failed to flush writer: %v", err)
-		return err
-	}
-
-	return nil
 }
 
-func ScrapeAndDownload() {
-	filePath := "app/videos/downloaded_titles.txt"
-	if err := ensureFileExists(filePath); err != nil {
-		log.Fatalf("Failed to ensure file exists: %v", err)
-		return
-	}
+func bestFormat(formats youtube.FormatList) *youtube.Format {
+	log.Println("Determining best video format...")
+	formats.Sort()
+	return &formats[0]
+}
 
-	downloadedTitles, err := readDownloadedTitles(filePath)
-	if err != nil {
-		log.Fatalf("Failed to read downloaded titles: %v", err)
-	}
+func downloadVideo(ctx context.Context, videoURL string) {
+	log.Println("Initiating video download...")
 
-	c := colly.NewCollector()
+	client := youtube.Client{}
 
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		title := e.Attr("title")
+	videoID, err := youtube.ExtractVideoID(videoURL)
+	handleFatalError(err, "extract video ID error")
 
-		if strings.Contains(title, "Bruce Falconer") && !downloadedTitles[title] {
-			videoURL := "https://www.youtube.com" + e.Attr("href")
-			cmd := exec.Command("youtube-dl", "-o", "/app/videos/%(title)s.%(ext)s", videoURL)
-			if err := cmd.Run(); err != nil {
-				log.Printf("Failed to download video: %v", err)
-				return
-			}
+	video, err := client.GetVideo(videoID)
+	handleFatalError(err, "get video error")
 
-			appendDownloadedTitle("/app/videos/downloaded_titles.txt", title)
+	stream, _, err := client.GetStream(video, bestFormat(video.Formats))
+	handleFatalError(err, "get video stream error")
 
-			log.Printf("Successfully downloaded and saved the video: %s", title)
+	sanitizedTitle := strings.Map(func(r rune) rune {
+		if r == os.PathSeparator || r == ':' {
+			return '_'
 		}
-	})
+		return r
+	}, video.Title)
 
-	if err := c.Visit("https://www.youtube.com/@goodgrief3308/videos"); err != nil {
-		log.Fatalf("Failed to visit the URL: %v", err)
-	}
-}
+	file, err := os.Create(sanitizedTitle + ".mp4")
+	handleFatalError(err, "create file error")
+	defer file.Close()
 
-func ensureFileExists(filePath string) error {
-	dir := filepath.Dir(filePath)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		log.Printf("Failed to create directory: %v", err)
-		return err
-	}
+	log.Printf("Downloading video: %s...\n", video.Title)
 
-	_, err := os.OpenFile(filePath, os.O_CREATE, 0644)
-	if err != nil {
-		log.Printf("Failed to create file: %v", err)
-		return err
-	}
+	log.Println("Download started at:", time.Now().Format(time.RFC1123))
 
-	return nil
+	_, err = io.Copy(file, stream)
+	handleFatalError(err, "download video error")
+
+	log.Println("Download completed at:", time.Now().Format(time.RFC1123))
+
+	log.Printf("Downloaded: %s\n", video.Title)
 }
 
 func main() {
-	ScrapeAndDownload()
+	log.Println("Starting application...")
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	videoURL, _, err := getLatestVideoURL(ctx)
+	handleFatalError(err, "get latest video URL error")
+
+	var history []string
+	file, err := os.ReadFile(historyFilePath)
+	if err == nil {
+		log.Println("Reading history file...")
+		json.Unmarshal(file, &history)
+	} else if !os.IsNotExist(err) {
+		log.Fatalf("Error reading history file: %v", err)
+	}
+
+	for _, v := range history {
+		if v == videoURL {
+			log.Println("No new videos to download.")
+			return
+		}
+	}
+
+	log.Println("Updating history file...")
+	history = append(history, videoURL)
+	file, err = json.Marshal(history)
+	handleFatalError(err, "Error marshalling history data")
+
+	err = os.WriteFile(historyFilePath, file, 0644)
+	handleFatalError(err, "Error writing history file")
+
+	downloadVideo(ctx, videoURL)
 }
